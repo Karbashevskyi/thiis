d6fcc541600d0c30bc1e11ab392d0613d6d226ff
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-03-28 16:42:30 +0200

    #33 update readme and add new method is.firefox()

---
+++ __test__/singel/firefox/apply.firefox.singel.test.ts
@@ -0,0 +1,35 @@
+import {is} from '../../../dist';
+import {dataForBadTesting, dataForGoodTesting} from './fixtures';
+
+
+describe('firefox singel: apply', () => {
+
+    const navigator =  {
+        userAgent: '[DEFAULT]'
+    }
+
+    beforeAll(() => {
+        Object.defineProperty(window, 'navigator', {value: navigator});
+    });
+
+    it.each(dataForGoodTesting)('is.firefox: Should true for $value', ({value}) => {
+        navigator.userAgent = value;
+        expect(is.firefox.apply({}, [])).toBe(true);
+    });
+
+    it.each(dataForBadTesting)('is.firefox: Should false for $value', ({value}) => {
+        navigator.userAgent = value;
+        expect(is.firefox.apply({}, [])).toBe(false);
+    });
+
+    it.each(dataForGoodTesting)('is.not.firefox: Should false for $value', ({value}) => {
+        navigator.userAgent = value;
+        expect(is.not.firefox.apply({}, [])).toBe(false);
+    });
+
+    it.each(dataForBadTesting)('is.not.firefox: Should true for $value', ({value}) => {
+        navigator.userAgent = value;
+        expect(is.not.firefox.apply({}, [])).toBe(true);
+    });
+
+});

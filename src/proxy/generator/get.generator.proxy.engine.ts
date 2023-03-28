eeced40fc99aabf9d4913a7d2ae740d411a51eb6
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-03-28 23:59:55 +0200

    Try to improve engine

--- src/proxy/generator/get.generator.proxy.engine.ts
+++ src/proxy/generator/get.generator.proxy.engine.ts
@@ -1,20 +1,19 @@
-import { proxyRecursive } from '../recursive';
-import { SymbolMethod } from '../../methods/symbol.method';
+import {proxyRecursive} from '../recursive';
+import {SymbolMethod} from '../../methods/symbol.method';
+import {isConfig} from '../../config';
 
 type proxyGeneratorGetType = (target: object, name: string, receiver: unknown) => ReturnType<typeof proxyRecursive>;
 
-export const packageName = 'thiis';
-
 export function proxyGeneratorGet(): proxyGeneratorGetType {
-  return (target, name, receiver) => {
-    if (SymbolMethod(name)) {
-      return undefined as any;
-    }
-    if (['valueOf', 'toString'].includes(name)) {
-      return packageName[name].bind(packageName);
-    }
-    return proxyRecursive(target, name, {
-      commandList: [],
-    });
-  };
+    return (target, name, receiver) => {
+        if (SymbolMethod(name)) {
+            return undefined as any;
+        }
+        if (['valueOf', 'toString'].includes(name)) {
+            return isConfig.packageName[name].bind(isConfig.packageName);
+        }
+        return proxyRecursive(target, name, {
+            commandList: [],
+        });
+    };
 }
--- src/proxy/recursive/index.ts
+++ src/proxy/recursive/index.ts
@@ -1,14 +1,15 @@
-import { predefinedMethods } from '../../methods';
-import { proxyRecursiveApply } from './apply.recursive.proxy.engine';
-import { ParamsProxyEngineInterface } from '../../interfaces/engine/proxy/params.proxy.engine.interface';
-import { CommandMixType } from '../../types/commands.type';
+import {predefinedMethods} from '../../methods';
+import {ParamsProxyEngineInterface} from '../../interfaces/engine/proxy/params.proxy.engine.interface';
+import {CommandMixType} from '../../types/commands.type';
+import {InstanceofMethod} from '../../methods/instanceof.method';
+import {getDecide} from './apply.recursive.proxy.engine';
 
 type proxyRecursiveGetType = (targetGet: any, nameGet: string) => ReturnType<typeof proxyRecursive>;
 
 export function proxyRecursiveGet(params: ParamsProxyEngineInterface): proxyRecursiveGetType {
-  return (targetGet, nameGet) => {
-    return proxyRecursive(targetGet[nameGet], nameGet, params);
-  };
+    return (targetGet, nameGet) => {
+        return proxyRecursive(targetGet[nameGet], nameGet, params);
+    };
 }
 
 /**
@@ -18,19 +19,29 @@ export function proxyRecursiveGet(params: ParamsProxyEngineInterface): proxyRecu
  * @param params ParamsProxyEngineInterface
  */
 export function proxyRecursive(target: object | string, name: string, params: ParamsProxyEngineInterface): object {
-  target = predefinedMethods[name] ?? name;
-  params.commandList.push(target as CommandMixType);
+    target = predefinedMethods[name] ?? name;
+    params.commandList.push(target as CommandMixType);
 
-  if (typeof target === 'string') {
-    if (['not', 'or'].includes(target)) {
-      target = predefinedMethods;
-    } else {
-      target = () => {};
+    if (typeof target === 'string') {
+        if (['not', 'or'].includes(target)) {
+            target = predefinedMethods;
+        } else {
+            target = () => {
+            };
+        }
     }
-  }
 
-  return new Proxy(target, {
-    get: proxyRecursiveGet(params),
-    apply: proxyRecursiveApply(params),
-  });
+    return new Proxy(target, {
+        get: proxyRecursiveGet(params),
+        apply: (notUsedTargetApply: any, thisArg: unknown, argumentList: unknown[] | unknown[][]): boolean => {
+            try {
+                return getDecide(params.commandList, argumentList);
+            } catch (e) {
+                if (InstanceofMethod(e, SyntaxError)) {
+                    throw e;
+                }
+                return false;
+            }
+        },
+    });
 }

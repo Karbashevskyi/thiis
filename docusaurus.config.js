607fba6a2d7c562e824f94f888b05b8c2d42adcd
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-03-30 22:36:34 +0200

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Karbashevskyi', // Usually your GitHub org/user name.
+  projectName: 'thiis', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

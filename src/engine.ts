d2cca1e7da820dc17997fb11fe2b2347d7d27cbb
Karba <ivan.karbashevskyi@gmail.com>
2023-10-29 02:09:11 +0100

    2.0.0-beta.1

--- src/engine.ts
+++ src/engine.ts
@@ -1,19 +1,19 @@
-import {InstanceofMethod} from './methods/instanceof.method';
-import {CommandType} from './types/commands.type';
-import {predefinedMethods} from './methods';
-import {isConfig} from './config';
+import { InstanceofMethod } from './methods/instanceof.method';
+import { CommandType } from './types/commands.type';
+import { predefinedMethods } from './methods';
+import { isConfig } from './config';
 
 function findInGlobalContext(command: string): undefined | CommandType {
-    try {
-        if (isConfig.useGlobalContext) {
-            return globalThis?.[command] ?? self?.[command] ?? window?.[command] ?? global?.[command] ?? command;
-        }
-    } catch (e) {
-        return undefined;
+  try {
+    if (isConfig.useGlobalContext) {
+      return globalThis?.[command] ?? self?.[command] ?? window?.[command] ?? global?.[command] ?? command;
     }
+  } catch (e) {
     return undefined;
+  }
+  return undefined;
 }
 
 export function getMethod(commandName: string): CommandType {
-    return predefinedMethods[commandName] ?? InstanceofMethod.bind({classRef: findInGlobalContext(commandName)});
+  return predefinedMethods[commandName] ?? InstanceofMethod.bind({ classRef: findInGlobalContext(commandName) });
 }

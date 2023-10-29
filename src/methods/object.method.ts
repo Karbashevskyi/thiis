b39455e89b377503bc7f13d037d26c1bc9539e7f
Karba <ivan.karbashevskyi@gmail.com>
2023-10-29 14:23:49 +0100

    2.0.1-beta.2

--- src/methods/object.method.ts
+++ src/methods/object.method.ts
@@ -1,5 +1,5 @@
-import {ArrayMethod} from './array.method';
-import {NullMethod} from './null.method';
+import { ArrayMethod } from './array.method';
+import { NullMethod } from './null.method';
 
 export function ObjectMethod<T extends object>(target: unknown, checkAny: boolean = false): target is T {
   return typeof target === 'object' && !ArrayMethod(target) && !NullMethod(target);

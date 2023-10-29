ecb756875dac6ffb4b75ce6daaa3698cd90537bd
Karba <ivan.karbashevskyi@gmail.com>
2023-10-29 14:59:30 +0100

    2.0.1-beta.3

--- src/methods/empty.method.ts
+++ src/methods/empty.method.ts
@@ -3,21 +3,21 @@ import { ObjectMethod } from './object.method';
 import { ArrayMethod } from './array.method';
 
 export function EmptyMethod<T extends object>(target: unknown): boolean {
-    // TODO find way to if prev method was ObjectMethod: is.object_not_empty
-    if (ObjectMethod<T>(target) || ArrayMethod<T>(target)) {
-        if ('size' in target) {
-            // @ts-ignore
-            return target.size <= 0;
-        }
-        for (const key in target) {
-            if (target.hasOwnProperty(key)) {
-                return false;
-            }
-        }
-        return true;
+  // TODO find way to if prev method was ObjectMethod: is.object_not_empty
+  if (ObjectMethod<T>(target) || ArrayMethod<T>(target)) {
+    if ('size' in target) {
+      // @ts-ignore
+      return target.size <= 0;
     }
-    if (StringMethod(target)) {
-        return target.trim()[0] === undefined;
+    for (const key in target) {
+      if (target.hasOwnProperty(key)) {
+        return false;
+      }
     }
-    return false;
+    return true;
+  }
+  if (StringMethod(target)) {
+    return target.trim()[0] === undefined;
+  }
+  return false;
 }

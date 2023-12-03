bdb2d778dad6a07c295452a7fd27ad8650b79323
Karba <ivan.karbashevskyi@gmail.com>
2023-12-03 21:18:43 +0100

    2.0.5-beta.0

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 import regexp from './regexp';
 
-
 const isNode = typeof process !== 'undefined' && process?.release?.name === 'node';
 const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
 
@@ -8,57 +7,57 @@ const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'u
 let globalContext: any = {};
 
 if (typeof globalThis !== 'undefined') {
-    globalContext = globalThis;
+  globalContext = globalThis;
 } else {
-    if (isNode) {
-        globalContext = global;
-    } else if (isBrowser) {
-        globalContext = window;
-    } else {
-        if (typeof self !== 'undefined') {
-            globalContext = self;
-        }
+  if (isNode) {
+    globalContext = global;
+  } else if (isBrowser) {
+    globalContext = window;
+  } else {
+    if (typeof self !== 'undefined') {
+      globalContext = self;
     }
+  }
 }
 
 let vendor = '';
 if (globalContext.navigator?.vendor) {
-    vendor = globalContext.navigator.vendor;
+  vendor = globalContext.navigator.vendor;
 }
 
 let userAgent = '';
 if (globalContext.navigator?.userAgent) {
-    userAgent = globalContext.navigator.userAgent;
+  userAgent = globalContext.navigator.userAgent;
 }
 
 let platform = '';
 if (globalContext.navigator?.platform) {
-    platform = globalContext.navigator.platform;
+  platform = globalContext.navigator.platform;
 } else if (globalContext.navigator?.userAgentData?.platform) {
-    platform = globalContext.navigator.userAgentData.platform;
+  platform = globalContext.navigator.userAgentData.platform;
 } else if (process?.platform) {
-    platform = process.platform;
+  platform = process.platform;
 }
 
 export const isConfig = {
-    packageName: 'thiis',
-    useGlobalContext: true,
-    globalContext,
-    error: {
-        enabled: true,
+  packageName: 'thiis',
+  useGlobalContext: true,
+  globalContext,
+  error: {
+    enabled: true,
+  },
+  definition: {
+    phone: 768,
+  },
+  state: {
+    vendor,
+    navigator: globalContext.navigator,
+    userAgent,
+    platform,
+    screen: {
+      width: globalContext.screen?.width ?? 0,
     },
-    definition: {
-        phone: 768,
-    },
-    state: {
-        vendor,
-        navigator: globalContext.navigator,
-        userAgent,
-        platform,
-        screen: {
-            width: globalContext.screen?.width ?? 0,
-        },
-        toString: Object.prototype.toString,
-    },
-    regexp,
+    toString: Object.prototype.toString,
+  },
+  regexp,
 };
